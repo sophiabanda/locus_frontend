ef347a6288f2e0558db0594f628ead8d36d3ddd2
Christopher Menendez <chris.menendez007@gmail.com>
2019-08-13 21:12:52 -0400

    Burger menu template with styles

---
+++ client/src/home/burger/burger.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './burger.css';
+import { stack as Menu } from 'react-burger-menu'
+
+class Burger extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+  }
+
+  render () {
+    // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+    return (
+      <Menu right width={ 230 }>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">Results</a>
+        <a id="contact" className="menu-item" href="/contact">About</a>
+      </Menu>
+    );
+     
+  }
+}
+
+export default Burger
